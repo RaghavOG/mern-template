53b0ba36e1eab9411e8da3e88f6dbe22a2609e51
RaghavOG <04raghavsingla28@gmail.com>
2024-10-20 16:41:07 +0530

    mern template

---
+++ README.md
@@ -0,0 +1,6 @@
+## Installation
+
+After installing the package, run the following command to set up the project structure:
+
+```bash
+npm run init
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold  p-2 m-2">This is the home page</h1>
+      <Button className="m-2">Click me</Button>
+    </div>
+  )
+}
+
+export default Home
